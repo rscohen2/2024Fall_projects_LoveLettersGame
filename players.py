984188836456496246c64a851fd2149eb2d73154
rscohen2 <rscohen2@illinois.edu>
2024-11-20 08:48:41 -0600

    add strategy 2 and main section gameplay run through

--- players.py
+++ players.py
@@ -83,7 +83,7 @@ class Player:
         self.players_hand = [card for card in self.players_hand if card != self.card_played]
         self.card_played = None
 
-    #TODO : incoporate the strategies into one function?
+    #TODO : incorporate the strategies into one function?
 '''
     def strategy_1(self, opponents_hand):
         if 'Guard' in self.players_hand:

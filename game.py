857d206f7df30d455b0dc2f389541c1eb168cbfe
namdini51 <donginn2@illinois.edu>
2024-11-20 20:42:56 -0600

    Updated rough draft for game.py

--- game.py
+++ game.py
@@ -19,6 +19,9 @@ Remarks: I try to leave comments next to the actual codes, but I also leave impo
     4. Needs more work based on each others' class logic
         --> for instance, I might use or need attributes and/or functions from different classes
     5. I think I'm about 60% done with my rough draft (not FINAL DRAFT) as of 6:30pm Nov.19 Tuesday
+    6. Rough draft finished (11:35 Nov.20)
+        --> there are still parts that need modification
+        --> currently have a import issue in the test execution code at the bottom (not sure how to handle this yet)
 """
 
 from players import *
@@ -28,27 +31,28 @@ from collections import Counter
 
 class Game:
 
-    current_deck = None
-    cards = None
-    card_played = None
-    cards_in_play = []
-    players = None
-    cards_played = None #  not sure about this
+    # TODO: I'm not sure if we need these class attributes (I feel like it is redundant)
+    # current_deck = None
+    # cards = None
+    # card_played = None
+    # cards_in_play = []
+    # players = None
+    # cards_played = None #  not sure about this
 
     def __init__(self, players):
         self.players = self.create_players(players) # creating player objects
         self.cards = [] # deck of cards
         self.remaining_cards = Counter() # Track remaining cards
-        # self.cards = current_deck(self.cards, self.cards_drawn)
         self.current_player_index = 0 # tracking turn for current player
         self.new_game()
-        # self.cards_played = [] # tracking played cards
-        # self.cards_in_play = []
+        self.cards_played = [] # tracking played cards TODO: isn't this redundant with player.card_played?
+        self.cards_in_play = [] # tracking cards currently in play TODO: not sure if we need this (maybe might need it when we are going to consider card memory/knowledge?)
 
-    def new_game(self): # moved this to class function from normal function (outside the class)
+
+    def new_game(self) -> None: # moved this to class function from normal function (outside the class)
         """
         Starts a new game by resetting (shuffling) card and player status
-        :return: cards
+        :return: None
         """
         # Shuffling the deck (deck object obtained)
         self.cards = [Guard()] * 5 + [Priest()] * 2 + [Baron()] * 2 + [Handmaid()] * 2 + [Prince()] * 2 + [King()] + [Countess()] + [Princess()]
@@ -56,23 +60,23 @@ class Game:
 
         # this part keeps track of the remaining cards
         self.remaining_cards = Counter()
-        for card in self.cards:
-            self.remaining_cards[card.__class__.__name__] += 1
+        for card in self.cards: # card is each card (subclass) object
+            self.remaining_cards[card.__class__.__name__] += 1 # didn't know how to access subclass names from other class object (referred to https://stackoverflow.com/questions/3314627/get-subclass-name)
 
         # resetting player status
         for player in self.players:
             player.players_hand = [] # empty player's hand
-            player.card_knowledge = {} # I changed this to dictionary (it is None in the players class module)
-            player.player_remaining = True   # This property isn't yet implemented in player class module
-            player.player_protected = False  # This property isn't yet implemented in player class module
+            # player.card_knowledge = {} # I changed this to dictionary (it is None in the players class module) TODO: we don't need this yet
+            player.player_remaining = True   # reset all players to be remained in the game
+            player.player_protected = False  # reset the protection effect (via Handmaid card)
+            player.card_played = None # reset played card TODO: should this be an empty list or None?
 
 
     def create_players(self, players_names: list[str]) -> list[Player]:
         """
         Function that creates players objects based on their names
         :param players_names:
-        :return:
-        # TODO: write a DocTest
+        :return: player objects
         """
         strategies = ["strategy_1", "strategy_2", "strategy_3"]
         player_objects = []
@@ -83,22 +87,23 @@ class Game:
         return player_objects
 
 
-    def play_turn(self):
+    def play_turn(self) -> None:
         """
         This function represents an individual player's turn as follows:
         1. draw a card
         2. choose card to play
         3. play the card
-        :return:
+        4. to the next player
+        :return: None
         """
         player = self.players[self.current_player_index]
-        if player.player_remaining and not player.player_protected: # TODO: need these two player class attributes (will be implemented soon)
+        if player.player_remaining and not player.player_protected:
             self.draw_a_card(player) # draw a card
-            card_index = player.card_to_play() # TODO: not sure if player.card_to_play() is accessible (maybe need a card_to_play function)
+            selected_card = player.card_to_play() # TODO: I changed the variable name card_index to selected_card since it seemed misleading
             target = self.choose_opponent(player) # randomly choose opponent
-            player.play_card(card_index, target) # play the chosen card TODO: not sure if player.play_card() is accessible maybe need a play_card function
+            player.play_card(selected_card, target) # play the chosen card
 
-        # to next player
+        # to next player (make sure the order is properly circulating (e.g., 1 -> 2 -> 3 / 2 -> 3 -> 1 / 3 -> 1 -> 2)
         self.current_player_index += 1
         if self.current_player_index >= len(self.players):
             self.current_player_index = 0
@@ -106,10 +111,14 @@ class Game:
 
     # I sort of merged and modified the original game_play_until_winner() and winner() into one function
     # Also I modified it as an instance function from class function
-    def play_until_winner(self):
+    # I didn't use the dictionary idea since I thought keeping track of remaining players list seemed more simple (implemented in self.track_remaining_players() function)
+    def play_until_winner(self) -> Player:
         """
+        This function is designed to determine the winner of the game in two scenarios:
+            1. Last player standing (i.e., other players got eliminated)
+            2. Deck out of cards -> player with highest value card wins
 
-        :return:
+        :return: return winner
         """
         self.new_game() # start a new game (reshuffle deck and restart player status)
 
@@ -117,24 +126,27 @@ class Game:
         for player in self.players:
             self.draw_a_card(player)
 
-        # TODO: complete the winner logic below
-        # play until either 1.one player standing or 2.deck gets empty
-        # if len(Game.players) != 1 or len(Game.cards) != 0:  # if no winner yet
-        #     round()
-        #
-        # # recursion of rounds until a winner is identified for that game
-        # else:
-        #     winner = Game.winner()
+        # keep playing turns until there is only one player standing OR until the deck is empty
+        while len(self.track_remaining_players()) > 1 and len(self.cards) > 0:
+            self.play_turn()
 
-            pass
+        # determine winner (2 cases)
+        remaining_players = self.track_remaining_players()
+
+        # 1. last player standing
+        if len(remaining_players) == 1:
+            return remaining_players[0]
+
+        # 2. player with highest card
+        return self.get_player_with_highest_card(remaining_players)
 
 
     # moved to instance function
-    def draw_a_card(self, player):
+    def draw_a_card(self, player) -> None:
         """
-        Draws a card randomly from the current deck
+        This function draws a card from the current deck
         :param player:
-        :return:
+        :return: None
         """
         if self.cards: # this is the deck of cards
             card = self.cards.pop() # removes drawn card from deck (i.e., deck decreases)
@@ -142,7 +154,12 @@ class Game:
 
 
     # moved to instance function
-    def choose_opponent(self, current_player):
+    def choose_opponent(self, current_player: Player): # TODO: not sure how to type annotate this function (specifically the return value)
+        """
+        This function randomly selects opponent who are 1) remaining in the game and 2) is not protected by handmaid card
+        :param current_player:
+        :return: opponent player or None
+        """
         available_opponent = []
         for player in self.players:
             if player != current_player and player.player_remaining and not player.player_protected:
@@ -154,177 +171,48 @@ class Game:
         return None
 
 
-    # @classmethod
-    # def game_play_until_winner(cls):
-    #     if len(Game.players) != 1 or len(Game.cards) != 0:  # if no winner yet
-    #         round()
-    #     # recursion of rounds until a winner is identified for that game
-    #     else:
-    #         winner = Game.winner()
-    #     return winner
-    #
-    # @classmethod
-    # def winner(cls):
-    #     if len(Player.players) == 1:
-    #         return Player.players[0]
-    #
-    #     elif len(Game.current_deck) == 0:
-    #         ending_hands = {}
-    #         for player in Player.players:
-    #             ending_hands[player].append(Player.players_hand.card_value)
-    #
-    #             # TODO : find correct syntax for adding to dict, its not the above pseudo code
-    #             current_max_value = 0
-    #             for player in ending_hands:
-    #                 if player.players_hand.card_value > current_max_value:
-    #                     current_max_value = player.players_hand.card_value
-    #                 # can we access who that player is from the class structure above
-    #                 winner = current_max_value
-    #                 return winner
+    def track_remaining_players(self) -> list[Player]:
+        """
+        This function tracks down the remaining players in the game
+        :param self
+        :return: list of remaining players
+        """
+        remaining_players = []
+        for player in self.players:
+            if player.player_remaining:
+                remaining_players.append(player)
+
+        return remaining_players
 
 
-# def deal_cards():
-#     """
-#     Deals cards randomly to each player prior to the first round
-#     :return:
-#     """
-#     Player.players_hand = []
-#     card_drawn = draw_a_card(cards_drawn, cards)
-#     Player1.players_hand.append(card_drawn)
-#
-#     # Player2.players_hand = []
-#     # card_drawn = draw_a_card(cards_drawn, cards)
-#     # Player2.players_hand.append(card_drawn)
-#     #
-#     # Player3.players_hand = []
-#     # card_drawn = draw_a_card(cards_drawn, cards)
-#     # Player3.players_hand.append(card_drawn)
+    # just modified a bit of our original winner() function
+    def get_player_with_highest_card(self, players: list[Player]) -> Player:
+        """
+        This function determines the player with the highest card
+        :return: player that has the card with the highest value
+        """
+        player_with_highest_card = None
+        highest_card_value = 0
+        for player in self.players:
+            if player.players_hand and player.players_hand[0].value > highest_card_value: # TODO: not sure if this is how accessing the card values works but I'm going to believe pycharm's autocompletion for now haha
+                highest_card_value = player.players_hand[0].value
+                player_with_highest_card = player
 
+        return player_with_highest_card
 
-"""
-I'm currently trying to reduce the functions below. I believe some functions can be merged or even unnecessary.
-"""
+# this is just a simple execution code to see if game.py is working (not a monte carlo)
+if __name__ == "__main__":
+    player_names = [Player('playStrategy1', 'Sarah'), Player('playStrategy2','Becca'), Player('playStrategy3','Andrew')]
+    game = Game(player_names)
 
-def current_deck(cards, cards_drawn):
-    """
-    keep track of the current deck, removing cards_played (discard pile) and cards_drawn (currently in players hands)
-    :param cards:
-    :param cards_drawn:
-    :return: cards that are in play
-    """
-    for card in cards:
-        if card in cards_drawn:
-            cards.remove(card)
-    return cards
+    winner = game.play_until_winner()
 
-def players_hand(player): # Isn't this in Player Class?
-    """
-    keep track of the players current hand
-    :param player:
-    :return:
-    """
-    players_hand = []
-    return players_hand
+    if winner:
+        print(f"{winner.name} wins!")
+    else:
+        print("\nSomething went wrong with the code")
 
-# def draw_a_card(cards_drawn, cards, players_hand):
-#     """
-#     Draws a card randomly from the current deck
-#     :param cards_drawn:
-#     :param cards:
-#     :return: card that is drawn
-#     """
-#     i = random.randint(0, 15)
-#     card_drawn = cards[i]
-#     cards_drawn.append(card_drawn)
-#     players_hand.append(card_drawn)
-#     return card_drawn, cards_drawn, players_hand
-
-
-# def deal_cards(self):
-#     """
-#     Deals cards randomly to each player prior to the first round
-#     :return:
-#     """
-#     for player in self.players:
-#         player.players_hand = []
-#         card_drawn = draw_a_card(cards_drawn, cards)
-#         player.players_hand.append(card_drawn)
-
-# def play_card():
-#     """
-#
-#     :return:
-#     """
-
-
-def player_is_out_of_the_round(player_out, players):
-    """
-    removes a player who is out of the round from the players list, and returns the players who are still in that round
-    :return: players who are still in the round
-    """
-    players = players.remove(player_out)
-    return players
-
-
-def list_opponents(players, current_player):
-    # TODO: check if opponent has handmaid card, then remove them from opponents list?
-
-    opponents = players.remove(current_player)
-    return opponents
-
-# def choose_opponent(opponents, opponent_card_in_play):
-#     # TODO: check if opponent has handmaid card, then remove them from opponents list?
-#     for opponent in opponents:
-#         if opponent_card_in_play == 'Handmaid':
-#     # TODO: implement opponent_card_in_play somehow in the player class??
-#             opponents.remove(opponent)
-#     opponent = random.choice(opponents)
-#     return opponent
-
-def update_cards_played(card_played, cards_played):
-    cards_played.append(card_played)
-    return cards_played, cards_played
-
-def clear_cards_in_play(cards_in_play):
-    cards_in_play = []
-    return cards_in_play
-
-def player_turn(player, Player):
-    card_drawn = draw_a_card(cards_drawn, cards)
-    player.players_hand.append(card_drawn)
-    move = player.strategy(self, opponents_hand)
-    return move
-
-
-def round():
-    for player in players:
-        player_turn(player, player.strategy)
-
-
-# def winner():
-#     if len(players) == 1:
-#         return players[0]
-#
-#     elif len(current_deck) = 0:
-#         ending_hands = {}
-#
-#         # players[0] > players [1]
-#         for player in players:
-#             ending_hands.append(player: player.players_hand.card_value)
-#             #TODO : find correct syntax for adding to dict, its not the above pseudo code
-#             current_max_value = 0
-#             for player in ending_hands:
-#                 if player.players_hand.card_value > current_max_value:
-#                 current_max_value = player.players_hand.card_value
-#                 #can we access who that player is from the class structure above
-#                 winner = current_max_value
-#                 return winner
-
-# #check if there is already a clear winner before implementing a round
-# def game_play_until_winner():
-#     if len(players) != 1 or len(cards) != 0: #if no winner yet
-#         round()
-#     #recursion of rounds until a winner is identified for that game
-#     else:
-#         winner = winner()
-#     return winner
+    # TODO: Currently getting the following error:
+    # ImportError: cannot import name 'choose_opponent' from partially initialized module 'game' (most likely due to a circular import)
+    # (C:\Users\namdi\PycharmProjects\IS597\2024Fall_projects_LoveLettersGame\game.py)
+    # I think this has to do with the game class and player class modules getting into a circular import

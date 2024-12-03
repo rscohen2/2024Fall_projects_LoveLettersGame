f47af81eaf6c7a8e26c7dd83eb4839e73d3249a4
namdini51 <donginn2@illinois.edu>
2024-12-03 00:29:49 -0600

    modified code to keep track of deck and cards

--- game.py
+++ game.py
@@ -39,10 +39,10 @@ class Game:
     # TODO: I'm not sure if we need these class attributes (I feel like it is redundant)
     # current_deck = None
     # cards = None
-    card_played = None
-    cards_in_play = []
-    players = None
-    cards_played = None #  not sure about this
+    # card_played = None
+    # cards_in_play = []
+    # players = None
+    # cards_played = None #  not sure about this
 
     def __init__(self, players):
         self.players = players # creating player objects
@@ -50,8 +50,8 @@ class Game:
         self.remaining_cards = Counter() # Track remaining cards
         self.current_player_index = 0 # tracking turn for current player
         self.new_game()
-        self.cards_played = [] # tracking played cards TODO: isn't this redundant with player.card_played?
-        self.cards_in_play = [] # tracking cards currently in play TODO: not sure if we need this (maybe might need it when we are going to consider card memory/knowledge?)
+        # self.cards_played = [] # tracking played cards TODO: isn't this redundant with player.card_played?
+        # self.cards_in_play = [] # tracking cards currently in play TODO: not sure if we need this (maybe might need it when we are going to consider card memory/knowledge?)
         # TODO: another property like self.card (for discarded pile) -> this will be even helpful for debugging purposes -> cards_played can be used for this (just give it another name)
 
     def new_game(self) -> None: # moved this to class function from normal function (outside the class)
@@ -61,38 +61,43 @@ class Game:
         """
         # Shuffling the deck (deck object obtained)
         self.cards = [c.Guard()] * 5 + [c.Priest()] * 2 + [c.Baron()] * 2 + [c.Handmaid()] * 2 + [c.Prince()] * 2 + [c.King()] + [c.Countess()] + [c.Princess()]
-        self.allcards = [i for i in self.cards] # added "allcards" to keep a list of all the cards later for the guess
         random.shuffle(self.cards)
 
         # this part keeps track of the remaining cards
         self.remaining_cards = Counter()
         for card in self.cards: # card is each card (subclass) object
-            self.remaining_cards[card.__class__.__name__] += 1 # didn't know how to access subclass names from other class object (referred to https://stackoverflow.com/questions/3314627/get-subclass-name)
+            card_name = card.__class__.__name__  # didn't know how to access subclass names from other class object (referred to https://stackoverflow.com/questions/3314627/get-subclass-name)
+            self.remaining_cards[card_name] += 1
 
-
-
-        # resetting player status
+        # # resetting player status
         for player in self.players:
-            player.players_hand = [] # empty player's hand
-            self.draw_a_card(player)
-            if len(player.players_hand) != 1:
-                print("potential error")
-            #it never stopped at the breakpoint above
-
-            # player.card_knowledge = {} # I changed this to dictionary (it is None in the players class module) TODO: we don't need this yet
-            player.player_remaining = True   # reset all players to be remained in the game
-            player.player_protected = False  # reset the protection effect (via Handmaid card)
-            player.card_played = None # reset played card TODO: should this be an empty list or None?
-
-            # player.card_knowledge = {}
-            # player.card_knowledge.update(self.players.remove(player) : '')
+            # self.draw_a_card(player)
+            player.players_hand = []
+            player.player_remaining = True
+            player.player_protected = False
             player.card_knowledge = {
                 opponent.name: [] for opponent in self.players if opponent != player
             }
-            #I gave chatgpt our prior code and error and asked what was wrong
 
-            #oh also here we need to deal cards when we start a new game!!
+        for player in self.players:
+            self.draw_a_card(player)
 
+        self.current_player_index = 0
+
+        # for player in self.players:
+        #     player.players_hand = [] # empty player's hand
+        #     # player.card_knowledge = {} # I changed this to dictionary (it is None in the players class module) TODO: we don't need this yet
+        #     player.player_remaining = True   # reset all players to be remained in the game
+        #     player.player_protected = False  # reset the protection effect (via Handmaid card)
+        #     # player.card_knowledge = {}
+        #     # player.card_knowledge.update(self.players.remove(player) : '')
+        #     player.card_knowledge = {
+        #         opponent.name: [] for opponent in self.players if opponent != player
+        #     }
+        #     #I gave chatgpt our prior code and error and asked what was wrong
+        #
+        #     #oh also here we need to deal cards when we start a new game!!
+        #     self.draw_a_card(player)
 
     def play_turn(self) -> None:
         """
@@ -103,34 +108,94 @@ class Game:
         4. to the next player
         :return: None
 
-
         """
+        # added to determine winner when deck is empty (the previous version kept returning an error when a player tried to guess a card when the deck is already empty)
+        if not self.cards:
+            print("We're out of cards! Compare cards with each other.")
+            return
+
         player = self.players[self.current_player_index]
+
         #if player.player_remaining and not player.player_protected:
-        if player.player_remaining:
-            if (len(player.players_hand)) != 1:
-                print('potential error?')
+        if not player.player_remaining:
+            self.current_player_index = (self.current_player_index + 1) % len(self.players)
+            return
+            # if (len(player.players_hand)) != 1:
+            #     print('potential error?')
             #this should be 1 above...
             #they should draw a card even if they have handmaid tho, so I changed this
-            self.draw_a_card(player) # draw a card
+            # self.draw_a_card(player) # draw a card
             # sanity check that the player should have 2 cards now
-            if (len(player.players_hand)) != 2:
-                print('error?')
-            selected_card = player.card_to_play() # TODO: I changed the variable name card_index to selected_card since it seemed misleading
-            target = self.choose_opponent(player) # randomly choose opponent
-            if player.strategy == "strategy_1": #adding guess for strategy 1 (always guess Princess)
-                print(self.allcards)
-                guess = [i for i in self.allcards if "Princess" in str(type(i))][0]
-            else: # else if not strategy1, then the guess will be random from all the cards in the deck
-                guess = self.remaining_cards[randint(0,len(self.remaining_cards))]
-            print(guess)
-            player.play_card(selected_card, target, guess) # play the chosen card
+            # if (len(player.players_hand)) != 2:
+            #     print('error?')
+
+        player.player_protected = False
+        self.draw_a_card(player)
+
+        selected_card = player.card_to_play() # TODO: I changed the variable name card_index to selected_card since it seemed misleading
+
+        target = None
+        if isinstance(selected_card, (c.Guard, c.Baron, c.King, c.Priest, c.Prince)):
+            target = self.choose_opponent(player)
+
+        if not selected_card:
+            print(f"{player.name} has no valid card to play.")
+            self.current_player_index = (self.current_player_index + 1) % len(self.players)
+            return
+
+        # Guard card requires a guess
+        guess = None
+        if isinstance(selected_card, c.Guard) and target:
+            guess = player.guess_card(self.get_remaining_card_list())
+
+        if target is None and not isinstance(selected_card, (c.Handmaid, c.Countess, c.Princess)):
+            print(f"No valid target for {player.name}. Turn skipped.")
+            self.current_player_index = (self.current_player_index + 1) % len(self.players)
+            return
+
+        selected_card.play_card(player, target, guess)
+        player.remove_card(selected_card)
+
+        # target = self.choose_opponent(player) # randomly choose opponent
+        # # TODO: I added the guessing logic for strategy 1 in player class
+        # guess = player.guess_card(self.get_remaining_card_list())
+
+        # if selected_card:
+            # selected_card_name = selected_card.__class__.__name__
+            # if target or isinstance(selected_card, c.Handmaid):
+            #     player.play_card(selected_card, target, guess)
+            # player.play_card(selected_card, target, guess)
+
+
+            # if player.strategy == "strategy_1": #adding guess for strategy 1 (always guess Princess)
+            #     print(self.allcards)
+            #     guess = [i for i in self.allcards if "Princess" in str(type(i))][0]
+            # else: # else if not strategy1, then the guess will be random from all the cards in the deck
+            #     guess = self.remaining_cards[randint(0,len(self.remaining_cards))]
+            # print(guess)
+            # if target or isinstance(selected_card, c.Handmaid):
+            #     player.play_card(selected_card, target, guess) # play the chosen card
+
 
         # to next player (make sure the order is properly circulating (e.g., 1 -> 2 -> 3 / 2 -> 3 -> 1 / 3 -> 1 -> 2)
         self.current_player_index += 1
         if self.current_player_index >= len(self.players):
             self.current_player_index = 0
 
+    # Added this function to properly get the remaining card list
+    def get_remaining_card_list(self):
+        """Returns a list of remaining cards."""
+        remaining_cards = []
+        for card_name, count in self.remaining_cards.items():
+            for _ in range(count):
+                remaining_cards.append(card_name)
+
+        for player in self.players:
+            if player.player_remaining:
+                for card in player.players_hand:
+                    remaining_cards.append(card.__class__.__name__)
+
+        return remaining_cards
 
     # I sort of merged and modified the original game_play_until_winner() and winner() into one function
     # Also I modified it as an instance function from class function
@@ -145,12 +210,17 @@ class Game:
         """
         self.new_game() # start a new game (reshuffle deck and restart player status)
 
-        # deal one card each for players
-        for player in self.players:
-            self.draw_a_card(player)
+        # for player in self.players:
+        #     self.draw_a_card(player)
+        #     player.player_remaining = True
+        #     player.player_protected = False
+
+        # # deal one card each for players
+        # for player in self.players:
+        #     self.draw_a_card(player)
 
         # keep playing turns until there is only one player standing OR until the deck is empty
-        while len(self.track_remaining_players()) > 1 and len(self.cards) > 0:
+        while self.count_remaining_players() > 1 and len(self.cards) > 0:
             self.play_turn()
 
         # determine winner (2 cases)
@@ -165,16 +235,22 @@ class Game:
 
 
     # moved to instance function
-    def draw_a_card(self, player) -> None:
+    def draw_a_card(self, player):
         """
         This function draws a card from the current deck
         :param player:
         :return: None
         """
-        if self.cards: # this is the deck of cards
+        if len(player.players_hand) < 2 and self.cards: # I modified this line to ensure there are only 2 or less cards in player's hand
             card = self.cards.pop() # removes drawn card from deck (i.e., deck decreases)
             player.players_hand.append(card)
 
+            card_name = card.__class__.__name__
+            if self.remaining_cards[card_name] > 0:
+                self.remaining_cards[card_name] -= 1
+
+            # return card
+        # return None
 
 
     # moved to instance function
@@ -187,14 +263,22 @@ class Game:
         """
         available_opponent = []
         for player in self.players:
-            if player != current_player and player.player_remaining and not player.player_protected:
+            if player != current_player and player.player_remaining and not player.player_protected and len(player.players_hand) > 0:
                 available_opponent.append(player)
 
+        # if len(available_opponent) > 0:
         if available_opponent:
             return random.choice(available_opponent) # TODO: maybe we can modify this later when we are going to consider card knowledge?
 
+        print("No available opponents to target")
         return None
 
+    def count_remaining_players(self):
+        count = 0
+        for player in self.players:
+            if player.player_remaining:
+                count += 1
+        return count
 
     def track_remaining_players(self) -> list[p.Player]:
         """
@@ -241,6 +325,7 @@ def create_players(player_names: list[str], strategies: list[str]) -> list[p.Pla
         player_objects.append(p.Player(name=name, strategy=strategy))
 
     return player_objects
+
 def run_the_sim(Sarah_win_count, Becca_win_count, Andrew_win_count):
     # this player creation part changed due to the create_players() function (moved outside the game class)
     player_list = ["Sarah", "Becca", "Andrew"]
@@ -250,8 +335,6 @@ def run_the_sim(Sarah_win_count, Becca_win_count, Andrew_win_count):
     game = Game(players)
 
     # new_game(game)
-
-
     winner = game.play_until_winner()
 
     if winner:
@@ -265,7 +348,7 @@ def run_the_sim(Sarah_win_count, Becca_win_count, Andrew_win_count):
         return winner.name
     else:
         print("\nSomething went wrong with the code")
-
+        return "Error"
 
 
 # # this is just a simple execution code to see if game.py is working (not a monte carlo)
